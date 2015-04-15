1b1e7e8fd8ce3257378697ff7ae987a768d3c0b5
renky1025 <renky.1025@gmail.com>
2015-04-15 16:40:37 +0800

    Create func.js

---
+++ func.js
@@ -0,0 +1,10 @@
+
+function switchLight(lightAction){
+    console.log(lightAction);
+    if(lightAction == 'off'){
+        document.body.style.backgroundColor='#000';
+    } else {
+        document.body.style.backgroundColor='#fff';
+    }
+}
+
